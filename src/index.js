db5530655911858a3ab1aa1246c9721db133677d
stavbers <reddevweb@gmail.com>
2022-06-08 22:35:26 +0300

    step two

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import {model} from './model'
+import {title, text, columns, image} from './templates'
+import './styles/main.css'
+
+
+
+const $site = document.querySelector('#site');
+
+model.forEach(block => {
+    let html = ''
+
+    if(block.type === 'title'){
+        html = title(block);
+    } else if (block.type === 'text') {
+        html = text(block);
+    }
+    else if (block.type === 'columns') {
+        html = columns(block);
+    }
+    else if (block.type === 'image') {
+        html = image(block);
+    }
+    $site.insertAdjacentHTML('beforeend', html);
+})
---
+++ src/model.js
@@ -0,0 +1,10 @@
+export const model = [
+    {type: 'title', value: 'Hello World from JS!!!!!'},
+    {type: 'text', value: 'here we go with some text'},
+    {type: 'columns', value: [
+            '11111111111',
+            '22222222222',
+            '3333333333333'
+        ]},
+    {type: 'image', value: './assets/3.jpg'}
+]
