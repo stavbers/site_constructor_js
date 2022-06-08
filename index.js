d68f2f65db2b6c9f731426612e0f13f6e4edd8f7
stavbers <reddevweb@gmail.com>
2022-06-08 20:49:39 +0300

    add start files and structure

---
+++ index.js
@@ -0,0 +1,13 @@
+const model = [
+    {type: 'title', value: 'Hello World from JS'},
+    {type: 'text', value: 'here we go with some text'},
+    {type: 'colums', value: [
+        '11111111111',
+            '22222222222',
+            '3333333333333'
+        ]}
+]
+
+const $site = document.querySelector('#site');
+
+
